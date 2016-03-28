9e5cf73ea1d1b5fdb8b399af2228daa07bb8ac94
kbrannan <brannan.kevin@deq.state.or.us>
2016-03-28 15:45:09 -0700

    got all the data and the aggregated values into a single tidy data.frame

--- r-files/precip-analysis.R
+++ r-files/precip-analysis.R
@@ -32,10 +32,15 @@ rm(df.tmp)
 ## annual sums
 df.ann.sums <- summaryBy(obs ~ year + station + par + period,
                          df.data, FUN = sum)
+names(df.ann.sums) <- c(names(df.ann.sums)[-5],"val")
 ## monthly sums
 df.mon.sums <- summaryBy(obs ~ month + year + station + par + period,
                          df.data, FUN = sum)
-
+names(df.mon.sums) <- c(names(df.mon.sums)[-6], "val")
+# monthly averages
+df.mon.ave <- summaryBy(obs.sum ~ month + station + par + period,
+                        df.mon.sums, FUN = mean)
+names(df.mon.ave) <- c(names(df.mon.ave)[-5], "val")
 # average annual (weight by number of obs per year)
 df.tmp <- summaryBy(obs ~ year + station + par + period,
                          df.data, FUN = c(sum,length))
@@ -43,38 +48,43 @@ df.tmp.prod <- cbind(df.tmp, prod = df.tmp$obs.sum * df.tmp$obs.length)
 df.tmp.length <- summaryBy(obs.length ~ period + station + period + par, df.tmp, FUN = sum)
 df.tmp.un <- merge(df.tmp.prod, df.tmp.length)
 df.tmp.div <- cbind(df.tmp.un, frac= df.tmp.un$prod/df.tmp.un$obs.length.sum)
-df.ann.aves <- summaryBy(frac ~ period + station + par, df.tmp.div, FUN = sum)
+df.ann.aves <- summaryBy(frac ~ station + par + period, df.tmp.div, FUN = sum)
+names(df.ann.aves) <- c(names(df.ann.aves)[-4], "val")
 rm(list=ls(pattern="df.tmp*"))
 
 ## put all data into single data.frame with form
-## var        date     month  year    src    val
-## ave_ann    NA       NA     NA      factor numerical
-## sum_ann    NA       NA     factor  factor numerical
-## ave_mon    NA       factor NA      factor numerical
-## sum_mon    NA       factor factor  factor numerical
-## sum_daily  POSIXct  factor facor   factor numerical
+## var      date     month  year    station par    period val
+## ann_ave  NA       NA     NA      factor  factor factor numerical
+## ann_sum  NA       NA     factor  factor  factor factor numerical
+## mon_ave  NA       factor NA      factor  factor factor numerical
+## mon_sum  NA       factor factor  factor  factor factor numerical
+## day_sum  POSIXct  factor facor   factor  factor factor numerical
 ##
 ## month as abrev Jan through Dec
 ## year as 4-digits
-## src either org or upd
-## ave annual
-df.prec <- data.frame(var = "ave_ann",
-                      date = NA,
-                      month = NA,
-                      year = NA,
-                      src = c("org","upd"),
-                      val = df.ann.means[1, ])
-## sum annual
-df.prec <- data.frame(df.prec,
-                      var = "sum_ann",
-                      date = NA,
-                      month = NA,
-                      year = 
-                      src = c("org","upd"),
-                      val = df.ann.means[1, ])
+## station is OR350145 or OR358182
+## par is PREC or PEVT
+## period is org or upd
+## quick fix of names for df.data
+names(df.data) <- c("date",names(df.data)[c(-1, -7)], "val")
+df.prec <- rbind(cbind(var = "ann_ave", date = NA, month = NA, year = NA,
+                       df.ann.aves),
+                 cbind(var = "ann_sum", date = NA, month = NA,
+                       df.ann.sums),
+                 cbind(var = "mon_ave", date = NA, month = df.mon.ave$month,
+                       year = NA,
+                       df.mon.ave[ , c("station", "par", "period", "val")]),
+                 cbind(var = "mon_sum", date = NA, 
+                       df.mon.sums),
+                 cbind(var = "day_sum", df.data)
+)
+
+## clean up
+rm(list=ls(pattern = "^df\\.")[-grep("df.prec",ls(pattern = "^df\\."))])
 
 ## plots 
 ##
+p.prec <- ggplot(data=df.prec)
 ## daily values
 p.daily.prec <- ggplot(data = df.upd.met.wdm)
 p.daily.prec <- p.daily.prec + geom_segment(aes(x = tmp.date, xend = tmp.date,
